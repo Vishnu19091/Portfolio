5d55763fbf6a7f1f9a324f22fc9d84efeac55ae2
NatpuEnean <natpuenean@gmail.com>
2025-11-16 10:09:47 +0530

    theme is moved from navbar

--- src/components/NavBar/NavigationBar.tsx
+++ src/components/NavBar/NavigationBar.tsx
@@ -3,20 +3,9 @@
 import * as React from "react";
 import { Link as ScrollLink } from "react-scroll";
 import Tooltip from "./tooltip";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
-import { Button } from "../ui/button";
 import "./NavBar.css";
 
 export default function NavBar() {
-  const { setTheme } = useTheme();
-
   return (
     <div className="w-full">
       <div className="mobile:w-[80%] lmobile:w-[56%] tablet:w-fit desktop:w-fit bottom-6 block fixed left-[50%] -translate-x-[50%] bg-black/30 dark:bg-white/10 rounded-2xl px-6 py-1 backdrop-blur-xl z-30 mx-auto">
@@ -25,8 +14,8 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
+              smooth
+              spy
               to="home"
               duration={300}
             >
@@ -44,10 +33,10 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
-              duration={300}
+              smooth
+              spy
               to="about"
+              duration={300}
             >
               <Tooltip message="About">
                 <img
@@ -63,16 +52,16 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
-              duration={300}
+              smooth
+              spy
               to="skills"
+              duration={300}
             >
               <Tooltip message="Skills">
                 <img
-                  className="h-7 w-7"
                   src="/assets/navbar/skills.svg"
                   alt="Skills"
+                  className="h-7 w-7"
                 />
               </Tooltip>
             </ScrollLink>
@@ -82,16 +71,16 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
-              duration={300}
+              smooth
+              spy
               to="projects"
+              duration={300}
             >
               <Tooltip message="Projects">
                 <img
-                  className="h-6 w-6"
                   src="/assets/navbar/projects.svg"
                   alt="Projects"
+                  className="h-6 w-6"
                 />
               </Tooltip>
             </ScrollLink>
@@ -101,17 +90,17 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
+              smooth
+              spy
               duration={300}
               offset={-300}
               to="education"
             >
               <Tooltip message="Education">
                 <img
-                  className="h-[1.8rem] w-[1.8rem]"
                   src="/assets/navbar/education.svg"
                   alt="Education"
+                  className="h-[1.8rem] w-[1.8rem]"
                 />
               </Tooltip>
             </ScrollLink>
@@ -121,16 +110,16 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
-              duration={300}
+              smooth
+              spy
               to="experience"
+              duration={300}
             >
               <Tooltip message="Experience">
                 <img
-                  className="h-6 w-6"
                   src="/assets/navbar/experience.svg"
                   alt="Experience"
+                  className="h-6 w-6"
                 />
               </Tooltip>
             </ScrollLink>
@@ -140,58 +129,20 @@ export default function NavBar() {
           <li className="hover:scale-110 transition-all duration-300 cursor-pointer ease-in-out">
             <ScrollLink
               activeClass="active"
-              smooth={true}
-              spy={true}
+              smooth
+              spy
+              to="contact"
               duration={300}
               offset={-550}
-              to="contact"
             >
               <Tooltip message="Contact">
                 <img
-                  className="h-6 w-6"
                   src="/assets/navbar/contact.svg"
                   alt="Contact"
+                  className="h-6 w-6"
                 />
               </Tooltip>
             </ScrollLink>
-          </li>
-
-          <li>
-            <Tooltip message="Change Theme">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="cursor-pointer"
-                  >
-                    <Sun className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                    <span className="sr-only">Toggle theme</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("light")}
-                  >
-                    Light
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("dark")}
-                  >
-                    Dark
-                  </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="cursor-pointer"
-                    onClick={() => setTheme("system")}
-                  >
-                    System
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </Tooltip>
           </li>
         </ul>
       </div>
--- src/components/SocialsBar/SocialButtons.tsx
+++ src/components/SocialsBar/SocialButtons.tsx
@@ -1,77 +1,192 @@
-// TODO1: add hackerrank profile
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
-// TODO2: render this component with props,
-// i.e stop creating the components individually
-// instead pass data to this component and let it
-// create the buttons separately
+type SocialId = "github" | "linkedin" | "leetcode" | "hackerrank";
 
-export default function SocialsButton() {
+type SocialLink = {
+  id: SocialId;
+  href: string;
+  label: string;
+};
+
+type SocialsButtonProps = {
+  links?: SocialLink[];
+  showThemeToggle?: boolean;
+};
+
+const iconClasses = "mobile:h-7 mobile:w-7 desktop:h-8 desktop:w-8";
+
+function GithubIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 1024 1024"
+      className={iconClasses}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
+    </svg>
+  );
+}
+
+function LinkedinIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 1024 1024"
+      className={iconClasses}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path>
+    </svg>
+  );
+}
+
+function LeetCodeIcon() {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 95 111"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={iconClasses}
+    >
+      <path
+        d="M68.0063 83.0664C70.5 80.5764 74.5366 80.5829 77.0223 83.0809C79.508 85.579 79.5015 89.6226 77.0078 92.1127L65.9346 103.17C55.7187 113.371 39.06 113.519 28.6718 103.513C28.6117 103.456 23.9861 98.9201 8.72653 83.957C-1.42528 74.0029 -2.43665 58.0749 7.11648 47.8464L24.9282 28.7745C34.4095 18.6219 51.887 17.5122 62.7275 26.2789L78.9048 39.362C81.6444 41.5776 82.0723 45.5985 79.8606 48.3429C77.6488 51.0873 73.635 51.5159 70.8954 49.3003L54.7182 36.2173C49.0488 31.6325 39.1314 32.2622 34.2394 37.5006L16.4274 56.5727C11.7767 61.5522 12.2861 69.574 17.6456 74.8292C28.851 85.8169 37.4869 94.2846 37.4969 94.2942C42.8977 99.496 51.6304 99.4184 56.9331 94.1234L68.0063 83.0664Z"
+        fill="#FFA116"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M41.1067 72.0014C37.5858 72.0014 34.7314 69.1421 34.7314 65.615C34.7314 62.0879 37.5858 59.2286 41.1067 59.2286H88.1245C91.6454 59.2286 94.4997 62.0879 94.4997 65.615C94.4997 69.1421 91.6454 72.0014 88.1245 72.0014H41.1067Z"
+        fill="#B3B3B3"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M49.9118 2.02335C52.3173 -0.55232 56.3517 -0.686894 58.9228 1.72277C61.494 4.13244 61.6284 8.17385 59.2229 10.7495L16.4276 56.5729C11.7768 61.552 12.2861 69.5738 17.6453 74.8292L37.4088 94.2091C39.9249 96.6764 39.968 100.72 37.505 103.24C35.042 105.761 31.0056 105.804 28.4895 103.337L8.72593 83.9567C-1.42529 74.0021 -2.43665 58.0741 7.1169 47.8463L49.9118 2.02335Z"
+        fill="white"
+      ></path>
+    </svg>
+  );
+}
+
+function HackerrankIcon() {
+  return (
+    <svg
+      viewBox="0 0 64 64"
+      xmlns="http://www.w3.org/2000/svg"
+      className={iconClasses}
+    >
+      <polygon points="32 2 8 16 8 48 32 62 56 48 56 16 32 2" fill="#00EA64" />
+      <rect x="24" y="18" width="6" height="28" fill="#000000" />
+      <rect x="34" y="18" width="6" height="28" fill="#000000" />
+    </svg>
+  );
+}
+
+function getIcon(id: SocialId) {
+  switch (id) {
+    case "github":
+      return <GithubIcon />;
+    case "linkedin":
+      return <LinkedinIcon />;
+    case "leetcode":
+      return <LeetCodeIcon />;
+    case "hackerrank":
+      return <HackerrankIcon />;
+    default:
+      return null;
+  }
+}
+
+const defaultLinks: SocialLink[] = [
+  {
+    id: "github",
+    href: "https://github.com/Vishnu19091",
+    label: "GitHub profile",
+  },
+  {
+    id: "linkedin",
+    href: "https://www.linkedin.com/in/vishnuvt1183",
+    label: "LinkedIn profile",
+  },
+  {
+    id: "leetcode",
+    href: "https://leetcode.com/u/vishnuvt1183/",
+    label: "LeetCode profile",
+  },
+  {
+    id: "hackerrank",
+    href: "https://www.hackerrank.com/vishnuvt1183",
+    label: "Hackerrank profile",
+  },
+];
+
+export default function SocialsButton({
+  links = defaultLinks,
+  showThemeToggle = true,
+}: SocialsButtonProps) {
+  const { setTheme } = useTheme();
+
   return (
     <div className="flex flex-row gap-4 items-center justify-center">
-      {/* GITHUB */}
-      <a
-        href="https://github.com/Vishnu19091"
-        className="hover:text-red-600 transition-colors duration-300 ease-in-out"
-        target="_blank"
-      >
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          strokeWidth="0"
-          viewBox="0 0 1024 1024"
-          className="mobile:h-7 mobile:w-7 desktop:h-8 desktop:w-8"
-          xmlns="http://www.w3.org/2000/svg"
+      {links.map((link) => (
+        <a
+          key={link.id}
+          href={link.href}
+          aria-label={link.label}
+          className="hover:text-red-600 transition-colors duration-300 ease-in-out"
+          target="_blank"
+          rel="noreferrer"
         >
-          <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
-        </svg>
-      </a>
+          {getIcon(link.id)}
+        </a>
+      ))}
 
-      {/* LINKEDIN */}
-      <a
-        href="http://www.linkedin.com/in/vishnuvt1183"
-        className="hover:text-red-600 transition-colors duration-300 ease-in-out"
-        target="_blank"
-      >
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          strokeWidth="0"
-          viewBox="0 0 1024 1024"
-          className="mobile:h-7 mobile:w-7 desktop:h-8 desktop:w-8"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path>
-        </svg>
-      </a>
-
-      {/* LEETCODE */}
-      <a href="https://leetcode.com/u/vishnuvt1183/" target="_blank">
-        <svg
-          width="30"
-          height="30"
-          viewBox="0 0 95 111"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="mobile:h-7 mobile:w-7 desktop:h-8 desktop:w-8"
-        >
-          <path
-            d="M68.0063 83.0664C70.5 80.5764 74.5366 80.5829 77.0223 83.0809C79.508 85.579 79.5015 89.6226 77.0078 92.1127L65.9346 103.17C55.7187 113.371 39.06 113.519 28.6718 103.513C28.6117 103.456 23.9861 98.9201 8.72653 83.957C-1.42528 74.0029 -2.43665 58.0749 7.11648 47.8464L24.9282 28.7745C34.4095 18.6219 51.887 17.5122 62.7275 26.2789L78.9048 39.362C81.6444 41.5776 82.0723 45.5985 79.8606 48.3429C77.6488 51.0873 73.635 51.5159 70.8954 49.3003L54.7182 36.2173C49.0488 31.6325 39.1314 32.2622 34.2394 37.5006L16.4274 56.5727C11.7767 61.5522 12.2861 69.574 17.6456 74.8292C28.851 85.8169 37.4869 94.2846 37.4969 94.2942C42.8977 99.496 51.6304 99.4184 56.9331 94.1234L68.0063 83.0664Z"
-            fill="#FFA116"
-          ></path>
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M41.1067 72.0014C37.5858 72.0014 34.7314 69.1421 34.7314 65.615C34.7314 62.0879 37.5858 59.2286 41.1067 59.2286H88.1245C91.6454 59.2286 94.4997 62.0879 94.4997 65.615C94.4997 69.1421 91.6454 72.0014 88.1245 72.0014H41.1067Z"
-            fill="#B3B3B3"
-          ></path>
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M49.9118 2.02335C52.3173 -0.55232 56.3517 -0.686894 58.9228 1.72277C61.494 4.13244 61.6284 8.17385 59.2229 10.7495L16.4276 56.5729C11.7768 61.552 12.2861 69.5738 17.6453 74.8292L37.4088 94.2091C39.9249 96.6764 39.968 100.72 37.505 103.24C35.042 105.761 31.0056 105.804 28.4895 103.337L8.72593 83.9567C-1.42529 74.0021 -2.43665 58.0741 7.1169 47.8463L49.9118 2.02335Z"
-            fill="white"
-          ></path>
-        </svg>
-      </a>
+      {showThemeToggle && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon" className="cursor-pointer">
+              <Sun className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => setTheme("light")}
+            >
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => setTheme("dark")}
+            >
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => setTheme("system")}
+            >
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </div>
   );
 }
