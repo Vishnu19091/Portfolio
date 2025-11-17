933751b43897cd655c66b21422a088d812c6e59d
Vishnu19091 <vishnusah368@gmail.com>
2025-11-17 09:16:40 +0530

    Add Resume && Open resume in blank page(no auto download) && Fix hackerrank svg

--- src/components/SocialsBar/Resume.tsx
+++ src/components/SocialsBar/Resume.tsx
@@ -6,14 +6,12 @@ export default function Resume() {
     const pdfUrl = "/assets/Vishnu_VT_Resume.pdf";
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "Vishnu_VT_Resume.pdf";
-    document.body.appendChild(link);
+    link.target = "blank";
     link.click();
-    document.body.removeChild(link);
   };
   return (
     <button
-      className={` ${FriendlyStrangerFont.className} mobile:text-xl dekstop:text-3xl text-red-600 border border-red-600 px-5 py-2 rounded-2xl hover:bg-red-600 hover:text-white hover:font-extrabold transition-all duration-300 ease-in-out`}
+      className={` ${FriendlyStrangerFont.className} mobile:text-xl dekstop:text-3xl text-red-600 border border-red-600 px-5 py-2 rounded-2xl hover:bg-red-600 hover:text-white hover:font-extrabold cursor-pointer transition-all duration-300 ease-in-out`}
       onClick={onButtonClick}
     >
       Resume
