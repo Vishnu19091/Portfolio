22f62d85c1e3d131d5c18b0873c08a879187f0e1
Vishnu19091 <vishnusah368@gmail.com>
2026-01-26 20:01:02 +0530

    ADD -> Cron-job Project to project section && Resume update

--- src/components/UI/Projects/projects.tsx
+++ src/components/UI/Projects/projects.tsx
@@ -2,52 +2,7 @@
 import { FriendlyStrangerFont } from "@/fonts/FriendlyStranger";
 import ProjectCard from "./ProjectCard";
 import { Reveal } from "@/components/Reveal";
-
-const projects = [
-  {
-    images: [
-      "/assets/projects/Task-management/authpage.png",
-      "/assets/projects/Task-management/emptypage.png",
-      "/assets/projects/Task-management/all.png",
-      "/assets/projects/Task-management/inprog.png",
-      "/assets/projects/Task-management/done.png",
-    ],
-    title: "Task Management using Nest.js & Next.js",
-    duration: "August 2025",
-    description:
-      "Task management app where authenticated users can create and manage their tasks.",
-    website: "https://task-management-nest-js.vercel.app",
-    repo: "https://github.com/Vishnu19091/Task-Management-NestJS",
-  },
-  {
-    images: [
-      "/assets/projects/portfolio/portfolio_1.png",
-      "/assets/projects/portfolio/portfolio_2.png",
-      "/assets/projects/portfolio/portfolio_3.png",
-    ],
-    title: "My Portfolio",
-    duration: "June 2025",
-    description: "My Portfolio built using NextJs, TypeScript, React.",
-    website: "#",
-    repo: "https://github.com/Vishnu19091/Portfolio",
-  },
-  {
-    images: [
-      "/assets/projects/Extension/Blocked_Domain_Page.png",
-      "/assets/projects/Extension/Network_Logger.png",
-      "/assets/projects/Extension/Pop-up-window.png",
-      "/assets/projects/Extension/Pop-up-window-with-menu.png",
-      "/assets/projects/Extension/Blocked-site.png",
-    ],
-    title: "Phishing Detection Extension",
-    duration: "April 2025 - Present",
-    description: `This is a browser extension-based project which detects phishing websites
-    in real-time and alerts the user, uses rule-based approach to effectively identify and
-    prevent phishing attacks, spam, and defaced websites. Users can also block and manage custom domains`,
-    website: "https://addons.mozilla.org/addon/anti-phishing-extension/",
-    repo: "https://github.com/Vishnu19091/Phishing_Detection_Extension/",
-  },
-];
+import { projectsData } from "./projectData";
 
 export default function Projects() {
   return (
@@ -74,9 +29,9 @@ export default function Projects() {
       </div>
       <section>
         <div className="mobile:w-full mobile:mx-auto desktop:w-[80%] mx-auto grid mobile:grid-cols-1 desktop:grid-cols-2 gap-4">
-          {projects.map((project, index) => (
+          {projectsData?.map((project) => (
             <ProjectCard
-              key={index}
+              key={project.repo}
               images={project.images}
               title={project.title}
               duration={project.duration}
